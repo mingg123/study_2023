c5dd13ef2d2bf5f85aca8cb47c30ddbd4626671d
mingg123 <spgb123spgb@gmail.com>
2024-07-28 23:20:11 +0900

    decorator pattern 예시 추가

---
+++ DesignPattern/pattern/decoratorPattern/Whip.java
@@ -0,0 +1,15 @@
+package decoratorPattern;
+
+public class Whip extends CondimentDecorator{
+    public Whip(Beverage beverage){
+        this.beverage = beverage;
+    }
+
+    public String getDescription() {
+        return beverage.getDescription() + ", 휘핑 크림";
+    }
+
+    public double cost() {
+        return beverage.cost() + .10;
+    }
+}
